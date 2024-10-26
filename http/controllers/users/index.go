7d7f52c7d18fa691407092a67dad6d2ef4ba663a
Vitor Gabriel Silva - PESSOAL <vitorgabrielsbo1460@gmail.com>
2024-10-26 13:03:38 -0300

    feat(users routes): added users base routes

--- http/controllers/users/index.go
+++ http/controllers/users/index.go
@@ -1,11 +1,48 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
+	"main/database"
+	"main/models"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Thrive default API route")
 	fmt.Println("testando")
+}
+
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	var users []models.Group
+
+	database.DB.Find(&users)
+	json.NewEncoder(w).Encode(users)
+}
+
+func GetUserByEmail(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	email := vars["email"]
+	var currentUser models.User
+	database.DB.First(&currentUser, email)
+
+	json.NewEncoder(w).Encode(currentUser)
+}
+
+func GetUserById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var currentUser models.User
+	database.DB.First(&currentUser, id)
+
+	json.NewEncoder(w).Encode(currentUser)
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var newUser models.User
+	json.NewDecoder(r.Body).Decode(&newUser)
+	database.DB.Create(&newUser)
+	json.NewEncoder(w).Encode(newUser)
 }
--- http/routes/users/route.go
+++ http/routes/users/route.go
@@ -7,6 +7,10 @@ import (
 )
 
 func RegisterUserRoutes(r *mux.Router) {
-    r.Use(middleware.ContetTypeMiddleware)
-    r.HandleFunc("/", controllers.Home).Methods("GET")
+	r.Use(middleware.ContetTypeMiddleware)
+	
+	r.HandleFunc("/", controllers.GetAllUsers).Methods("GET")
+	r.HandleFunc("/", controllers.CreateUser).Methods("POST")
+	r.HandleFunc("/get-user-by-email/{email}", controllers.GetUserByEmail).Methods("GET")
+	r.HandleFunc("/get-user-by-id/{id}", controllers.GetUserById).Methods("GET")
 }
